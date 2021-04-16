98d5dd93937998910722fb68a9a11e5695ddb31d
hankgood95 <hankgood958@gmail.com>
2021-04-16 19:33:31 +0900

    7시33분 커밋간당!

---
+++ src/screen/cafe_info/cafeInfo.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+
+function DetailsScreen({ navigation }) {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>카페 정보글 화면이얌</Text>
+		</View>
+	);
+}
+export default DetailsScreen;
